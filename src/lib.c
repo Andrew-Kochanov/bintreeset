adbe3b0209cd0cc2dafa0f24d1240443e8e145fb
maxicot <st140678@student.spbu.ru>
2026-02-21 15:02:57 +0300

    Begin work

--- src/lib.c
+++ src/lib.c
@@ -1,6 +1,7 @@
 #include "bst_sized.h"
 #include <assert.h>
 #include <stdlib.h>
+#include <errno.h>
 
 typedef struct BSTNode {
     int value;
@@ -17,3 +18,33 @@ typedef struct BST {
 } BST;
 
 static_assert(sizeof(BST) == sizeof(BSTStandin__));
+
+int bstInsert(BST tree, int value)
+{
+    BSTNode* node = tree->root;
+
+    BSTNode* next = node.left;
+
+    while (next != nullptr) {
+        if (value < node->value) {
+            next = node.left;
+        } else {
+            next = node.right;
+        }
+    }
+
+    BSTNode* newNode = malloc(sizeof(BSTNode));
+
+    if (newNode == nullptr) {
+        return ENOMEM;
+    }
+
+    newNode = {
+        .value = value;
+        .parent = node;
+        .left = nullptr;
+        .right = nullptr;
+    };
+
+    node.left = newNode;
+}
