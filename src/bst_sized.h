fe6112e1bf01ac25f6391e3bcaf020f8a5d3df24
maxicot <st140678@student.spbu.ru>
2026-02-21 14:24:05 +0300

    Initial code

---
+++ src/bst_sized.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+
+typedef struct BSTNodeStandin__ {
+    uint8_t _dummy[32];
+} BSTNodeStandin__;
+
+typedef struct BSTStandin__ {
+    uint8_t _dummy[16];
+} BSTStandin__;
---
+++ src/lib.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <assert.h>
+#include "bst_sized.h"
+
+typedef struct BSTNode {
+    int value;
+    struct BSTNode* parent;
+    struct BSTNode* left;
+    struct BSTNode* right;
+} BSTNode;
+
+static_assert(sizeof(BSTNode) == sizeof(BSTNodeStandin__));
+
+typedef struct BST {
+    BSTNode* root;
+    size_t cardinality;
+} BST;
+
+static_assert(sizeof(BST) == sizeof(BSTStandin__));
