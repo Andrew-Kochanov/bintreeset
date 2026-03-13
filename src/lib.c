d2464b0444d132fd4a802e50c3527837bf1cb974
maxicot <st140678@student.spbu.ru>
2026-03-14 01:28:04 +0300

    Initial implementation for tasks A, B and C

--- src/lib.c
+++ src/lib.c
@@ -1,7 +1,8 @@
 #include "bst_sized.h"
 #include <assert.h>
-#include <stdlib.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct BSTNode {
     int value;
@@ -19,17 +20,20 @@ typedef struct BST {
 
 static_assert(sizeof(BST) == sizeof(BSTStandin__));
 
-int bstInsert(BST tree, int value)
+int bstInsert(BST* tree, int value)
 {
-    BSTNode* node = tree->root;
+    BSTNode** insert = &tree->root;
+    BSTNode* parent = nullptr;
 
-    BSTNode* next = node.left;
+    while (*insert != nullptr) {
+        parent = *insert;
 
-    while (next != nullptr) {
-        if (value < node->value) {
-            next = node.left;
+        if (value < (**insert).value) {
+            insert = &(**insert).left;
+        } else if (value > (**insert).value) {
+            insert = &(**insert).right;
         } else {
-            next = node.right;
+            return EEXIST;
         }
     }
 
@@ -39,12 +43,148 @@ int bstInsert(BST tree, int value)
         return ENOMEM;
     }
 
-    newNode = {
-        .value = value;
-        .parent = node;
-        .left = nullptr;
-        .right = nullptr;
+    *newNode = {
+        .value = value,
+        .parent = parent,
+        .left = nullptr,
+        .right = nullptr
     };
 
-    node.left = newNode;
+    *insert = newNode;
+    tree->cardinality++;
+
+    return 0;
+}
+
+bool bstContains(BST* tree, int value)
+{
+    BSTNode** node = &tree->root;
+
+    while (*node != nullptr) {
+        if (value < (**node).value) {
+            node = &(**node).left;
+        } else if (value > (**node).value) {
+            node = &(**node).right;
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void bstNodeFree(BSTNode** node)
+{
+    if (node == nullptr) {
+        return;
+    }
+
+    bstNodeFree(&(**node).left);
+    bstNodeFree(&(**node).right);
+    free(*node);
+    *node = nullptr;
+}
+
+void bstFree(BST* tree)
+{
+    tree->cardinality = 0;
+    bstNodeFree(tree->root);
+    tree->root = nullptr;
+}
+
+void bstInorderInner(const BSTNode* node)
+{
+    if (node == nullptr) {
+        return;
+    }
+
+    bstInorderInner(node->left);
+    printf("%d ", node->value);
+    bstInorderInner(node->right);
+}
+
+void bstInorder(const BST* tree)
+{
+    bstInorderInner(tree->root);
+    printf("\n");
+}
+
+void bstPreorderInner(const BSTNode* node)
+{
+    if (node == nullptr) {
+        return;
+    }
+
+    printf("%d ", node->value);
+    bstPreorderInner(node->left);
+    bstPreorderInner(node->right);
+}
+
+void bstPostorder(const BST* tree)
+{
+    bstPreorderInner(tree->root);
+    printf("\n");
+}
+
+void bstPostorderInner(const BSTNode* node)
+{
+    if (node == nullptr) {
+        return;
+    }
+
+    bstPostorderInner(node->left);
+    bstPostorderInner(node->right);
+    printf("%d ", node->value);
+}
+
+void bstPostorder(const BST* tree)
+{
+    bstPostorderInner(tree->root);
+    printf("\n");
+}
+
+size_t bstNodeHeight(const BSTNode* node)
+{
+    if (node == nullptr) {
+        return 0;
+    }
+
+    size_t left = bstNodeHeight(node->left);
+    size_t right = bstNodeHeight(node->right);
+
+    return 1 + (left > right ? left : right);
+}
+
+size_t bstHeight(const BST* tree)
+{
+    return bstNodeHeight(tree->root);
+}
+
+size_t bstSize(const BST* tree)
+{
+    return tree->cardinality;
+}
+
+// unsound for an empty tree
+int bstMin(const BST* tree)
+{
+    const BSTNode* node = tree->root;
+
+    while (node->left != nullptr) {
+        node = node->left;
+    }
+
+    return node->value;
+}
+
+// unsound for an empty tree
+int bstMax(const BST* tree)
+{
+    const BSTNode* node = tree->root;
+
+    while (node->right != nullptr) {
+        node = node->right;
+    }
+
+    return node->value;
 }
